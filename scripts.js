647aa980814bc68d52e2f69bf862076434376d2d
abm-jomblang <152235229+abm-jomblang@users.noreply.github.com>
2023-11-28 16:34:50 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -32,7 +32,7 @@ productsCollection.once("value").then(function(snapshot) {
       <div class="card shadow">
         <img src="${product.imageurl}" class="card-img-top" alt="${product.name}">
         <div class="card-body">
-          <h5 class="card-title mb-1">${product.name}</h5>
+          <h5 class="card-title mb-1 text-uppercase">${product.name}</h5>
           <p class="card-text mt-1"><b>${formattedHarga}/${product.satuan}</b></p>
           <p class="card-text">${product.deskripsi}</p>
         
@@ -46,6 +46,17 @@ productsCollection.once("value").then(function(snapshot) {
     productsContainer.appendChild(productCard);
   });
 });
+
+if (localStorage.getItem('cartProducts')) {
+  cartProducts = JSON.parse(localStorage.getItem('cartProducts'));
+  updateCartView();
+}
+
+// Fungsi untuk menyimpan cartProducts ke localStorage
+function saveCartToLocalStorage() {
+  localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
+}
+
 
 function addToCart(productId) {
   const quantityInput = document.getElementById(`quantity-${productId}`);
@@ -73,6 +84,7 @@ function addToCart(productId) {
     }
 
     updateCartView();
+      saveCartToLocalStorage();
 
     // Set nilai input kembali ke nilai awal (biasanya nol)
     quantityInput.value = 0;
@@ -105,8 +117,10 @@ function removeFromCart(productId) {
 
         // Update tampilan keranjang setelah penghapusan
         updateCartView();
+        saveCartToLocalStorage();
     }
 }
+
 function pesanBarang() {
   const cartItems = Array.from(document.querySelectorAll('#cart-list li'));
 
@@ -114,7 +128,7 @@ function pesanBarang() {
     const arrbulan = ["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];
     const date = new Date();
     const waktu = `${date.getDate()}-${arrbulan[date.getMonth()]}-${date.getFullYear()}`;
-    const nomorWhatsApp = '6285227795539';
+    const nomorWhatsApp = '62882005619735';
     const hed = '\nQty - Nama Barang\n==================\n';
     
     const pesan = `----~~~--- ABM ---~~~----\nTanggal: ${waktu}\nPesanan Hari Ini:\n•••••••••©2023••••••••••${hed}`;
@@ -122,7 +136,15 @@ function pesanBarang() {
     const detailPesanan = cartItems.map(item => item.querySelector('span').textContent).join('\n');
     const waLink = `https://wa.me/${nomorWhatsApp}?text=${encodeURIComponent(pesan + detailPesanan + tag)}`;
     window.open(waLink, '_blank');
+
+    // Hapus cookie setelah pesanan dibuat
+    deleteCookie('cartCookie');
   } else {
     alert('Keranjang belanja kosong. Tambahkan barang terlebih dahulu.');
   }
+}
+
+// Fungsi untuk menghapus cookie
+function deleteCookie(name) {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
 }
